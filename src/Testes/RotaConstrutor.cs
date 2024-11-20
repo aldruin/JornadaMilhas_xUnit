0521b8741604e668f00fa805762e5afe79b79012
aldruin <abl.souza97@gmail.com>
2024-11-20 08:10:42 -0300

    update rota testes

---
+++ src/Testes/RotaConstrutor.cs
@@ -0,0 +1,42 @@
+﻿using JornadaMilhasV1.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Test;
+public class RotaConstrutor
+{
+
+    [Fact]
+    public void RetornaRotaValidaDeAcordoComDadosDeEntrada()
+    {
+        //arrange
+        var rotaOrigem = "Campo Mourão";
+        var rotaDestino = "Curitiba";
+
+        //act
+        Rota rota = new Rota(rotaOrigem, rotaDestino);     
+
+        //assert
+        Assert.True(rota.EhValido);
+    }
+
+    [Fact]
+    public void RetornaRotaInvalidaDeAcordoComDadosDeEntradaNulosOuVazios()
+    {
+        //arrange
+        var rotaOrigem = "";
+        var rotaDestino = "";
+
+        //act
+        Rota rota = new Rota(rotaOrigem, rotaDestino);
+
+        //assert
+        Assert.Contains("A rota não pode possuir um destino nulo ou vazio.", rota.Erros.Sumario);
+        Assert.False(rota.EhValido);
+    }
+
+
+}
